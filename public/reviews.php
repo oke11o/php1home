42473418da687f7006de93c266ef4adabff27196
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2018-12-09 21:21:37 +0300

    Unit6. Mysqli escape string

--- public/reviews.php
+++ public/reviews.php
@@ -7,13 +7,17 @@ $search = $_GET['search'] ?? '';
 if ($_POST && isset($_POST['name']) && isset($_POST['text'])) {
     $name = $_POST['name'];
     $text = $_POST['text'];
-    $sql = sprintf("INSERT INTO reviews (name, text) VALUES ('%s', '%s')", $name, $text);
+    $sql = sprintf(
+        "INSERT INTO reviews (name, text) VALUES ('%s', '%s')",
+        mysqli_real_escape_string($mysqlConnect, (string)htmlspecialchars(strip_tags($name))),
+        mysqli_real_escape_string($mysqlConnect, (string)htmlspecialchars(strip_tags($text)))
+    );
     mysqli_query($mysqlConnect, $sql);
     if (mysqli_error($mysqlConnect)) {
         die(mysqli_error($mysqlConnect));
     }
-//    $id = mysqli_insert_id($mysqlConnect);
-    header('Location: reviews.php');
+    $id = mysqli_insert_id($mysqlConnect);
+    header('Location: reviews.php?new_id='.$id);
     die;
 }
 
